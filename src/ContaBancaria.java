3340215e3303509a9943d216b221fd553672eecb
GABrambila <brambila.gabriel26@gmail.com>
2023-05-25 23:30:13 -0300

    Initial commit AtividadeSwingHeranca1

---
+++ src/ContaBancaria.java
@@ -0,0 +1,40 @@
+public class ContaBancaria {
+
+    private String cliente;
+    private Integer num_conta;
+    private float saldo;
+
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public Integer getNum_conta() {
+        return num_conta;
+    }
+
+    public void setNum_conta(Integer num_conta) {
+        this.num_conta = num_conta;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+}
